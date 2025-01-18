0a4f01653be9ed24774d94a52a052e8f2846a22c
vaishnavPatange <patangevaishnav5@gmail.com>
2025-01-18 23:45:24 +0530

    Add project files.

---
+++ Migrations/20250118125419_Change FK.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Categories_CategoryId1",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_CategoryId1",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId1",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_CategoryId",
+                table: "Transactions",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Categories_CategoryId",
+                table: "Transactions",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Categories_CategoryId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_CategoryId",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryId",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId1",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_CategoryId1",
+                table: "Transactions",
+                column: "CategoryId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Categories_CategoryId1",
+                table: "Transactions",
+                column: "CategoryId1",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
